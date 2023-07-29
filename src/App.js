69e513591473ece3cc1ebe2f0ac3b4e86320a36e
Ahmet Ustun <austu@softserveinc.com>
2023-07-29 17:03:26 +0300

    UC#10 Implement regex

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+const regexFunction = (string, length) => {
+  const hasSpace = /\s/g;
+
+  if (string.length > length || hasSpace.test(string)) {
+    return false;
+  }
+
+  const pattern = /[a-zA-Z0-9`!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?~]+/g;
+
+  return pattern.test(string);
+};
 
 function App() {
+  const [text, setText] = useState();
+
+  const onChange = (event) => {
+    setText(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" value={text} onChange={onChange} />
+      <p>{text}</p>
+      {text && <p>{regexFunction(text, 8) ? "Valid" : "Invalid"}</p>}
     </div>
   );
 }
